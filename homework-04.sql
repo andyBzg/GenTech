9eac93acc2c1d551ef6a80d4e777d33a54cae021
andyBzg <andr.bzg@gmail.com>
2023-05-09 19:53:56 +0200

    Initial commit

---
+++ homework-04.sql
@@ -0,0 +1,35 @@
+В рамках БД "песочница" напишите след/запросы:
+
+1. Вывести количественное распределение товаров по поставщикам 
+(название компании, ко-во товаров)
+
+SELECT 
+	Suppliers.SupplierName, 
+	COUNT(*) AS total_products
+	FROM Products
+JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID
+GROUP BY Suppliers.SupplierName
+ORDER BY total_products DESC
+
+
+2. Вывести категорию, где больше всего товаров 
+(название категории, ко-во товаров)
+
+SELECT 
+	CategoryName,
+	COUNT(*) as total_products
+from Products
+JOIN Categories ON Products.CategoryID=Categories.CategoryID
+GROUP BY CategoryName
+ORDER BY total_products DESC
+LIMIT 1
+
+
+3. Вывести сумму каждого заказа (номер_заказа, сумма)
+
+SELECT 
+	OrderID, 
+	SUM(Price) AS total_price 
+FROM OrderDetails
+JOIN Products ON OrderDetails.ProductID=Products.ProductID
+GROUP BY OrderID
